4c9153140bfbc527a6093aa684940326fdd166f1
Danilo <danilo.franca01@gmail.com>
2017-10-23 17:34:14 -0300

    Teste no layout

--- telaPrincipal.py
+++ telaPrincipal.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 janelaLogin = tk.Tk()
-janelaLogin.title("Login")
+janelaLogin.title("Medical Manager")
 janelaLogin["bg"] = "#cbfbfe"
 janelaLogin.geometry("800x400+300+300")
-#tk.Label(janelaLogin, text="Login: ").pack()
-#tk.Label(janelaLogin, text="Senha: ").pack()
+bordaDireita  = tk.Label(janelaLogin, background = "Blue",height = 400).grid(row = 0,column=0)
+bordaEsquerda  = tk.Label(janelaLogin, background = "Blue",height = 400).grid(row = 0,column=1,sticky = NW)
+
 janelaLogin.mainloop()
